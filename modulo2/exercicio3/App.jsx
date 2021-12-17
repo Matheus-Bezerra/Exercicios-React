d64aa0df8acf18dc1e83d1a8a6022396ae8f12af
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-12-17 18:20:22 -0300

    Exercicio 3 do mod 2 resolvido com sucesso

--- modulo2/exercicio3/App.jsx
+++ modulo2/exercicio3/App.jsx
@@ -1,6 +1,7 @@
 // Crie um formulário, onde toda vez que for enviado
 // pegue o valor do input e mostre com os valores anteriores, como se fosse mensagens já enviadas
 // Após isso selecione o elemento html, o ref e utilize focus após o envio
+// Simulando como se fosse um chat de rede social
 
 const App = () => {
   const [comentarios, setComentarios] = React.useState([]);
@@ -9,13 +10,36 @@ const App = () => {
 
   function handleSubmit(event) {
     event.preventDefault();
+    inputEl.current.focus();
+    const value = inputEl.current.value;
+    if (value.trim()) {
+      setComentarios([...comentarios, value]);
+      setInput('');
+    }
+  }
+
+  function handleChange({ target }) {
+    setInput(target.value);
   }
   return (
     <React.Fragment>
       <form onSubmit={handleSubmit}>
         <label htmlFor="comentario">Comentarios: </label>
-        <input type="text" id="comentario" ref={inputEl} />
+        <input
+          type="text"
+          id="comentario"
+          ref={inputEl}
+          value={input}
+          onChange={handleChange}
+        />
         <button>Enviar</button>
+        <br />
+        <br />
+        <ul>
+          {comentarios.map((comentario) => (
+            <li key={comentario}>{comentario}</li>
+          ))}
+        </ul>
       </form>
     </React.Fragment>
   );
